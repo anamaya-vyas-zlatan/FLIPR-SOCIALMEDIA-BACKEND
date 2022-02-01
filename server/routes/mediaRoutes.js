ca1f3b34d6b6c89582f5b042233efeda849dcfd2
ANAMAYA VYAS <68732493+anamaya-vyas-zlatan@users.noreply.github.com>
2022-02-01 21:48:37 +0530

    Update mediaRoutes.js

--- server/routes/mediaRoutes.js
+++ server/routes/mediaRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import upload from "../utils/multer.js";
 import uploadMedia from "../controllers/media/uploadMedia.js";
+import uploadMultiple from "../controllers/media/uploadMultiple.js";
 import deleteMedia from "../controllers/media/deleteMedia.js";
 
 const router = express.Router();
 
 router.post("/uploadMedia", upload.single("image"), uploadMedia);
+
+router.post("/uploadMultiple", upload.single("image"), uploadMultiple);
 
 router.delete("/deleteMedia/:id", deleteMedia);
 
