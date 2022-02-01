f02b556d5035e1d922ebfc772512026e12048555
ANAMAYA VYAS <68732493+anamaya-vyas-zlatan@users.noreply.github.com>
2022-02-01 21:49:07 +0530

    Update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,17 +1,39 @@
 import express from "express";
-import createPublicPost from "../controllers/post/publicPost.js";
-import createPrivatePost from "../controllers/post/privatePost.js";
+import upload from "../utils/multer.js";
+import getPost from "../controllers/post/getPost.js";
+import createPost from "../controllers/post/createPost.js";
+import sharePost from "../controllers/post/sharePost.js";
+import likePost from "../controllers/post/likePost.js";
+import unlikePost from "../controllers/post/unlikePost.js";
+import addComment from "../controllers/post/addComment.js";
+import deleteComment from "../controllers/post/deleteComment.js";
+import addReply from "../controllers/post/addReply.js";
+import likeComment from "../controllers/post/likeComment.js";
+import unlikeComment from "../controllers/post/unlikeComment.js";
+import getComments from "../controllers/post/getComments.js";
+
+import { verifyLogin } from "../middlewares/user.middleware.js";
 
 const router = express.Router();
 
-router.get("/:user_id/:post_id");
+router.post("/create", upload.array("image"), verifyLogin, createPost);
 
-router.post("/public/:user_id", createPublicPost);
+router.patch("/like/:id", verifyLogin, likePost);
 
-router.post("/private/:user_id", createPrivatePost);
+router.patch("/unlike/:id", verifyLogin, unlikePost);
 
-router.delete("/:user_id/:post_id");
+router.patch("/addComment/:id", verifyLogin, addComment);
 
-router.patch("/:user_id/:post_id");
+router.patch("/deleteComment/:id", verifyLogin, deleteComment);
+
+router.patch("/addReply/:id", verifyLogin, addReply);
+
+router.patch("/likeComment/:id", verifyLogin, likeComment);
+
+router.patch("/unlikeComment/:id", verifyLogin, unlikeComment);
+
+router.get("/getComments/:post_id/:page/:perPage", verifyLogin, getComments);
+
+router.get("/:id", verifyLogin, getPost);
 
 export default router;
