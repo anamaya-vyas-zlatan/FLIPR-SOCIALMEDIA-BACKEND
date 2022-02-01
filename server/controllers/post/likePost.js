834b315caa2fa309f858652227d020c01df30d14
ANAMAYA VYAS <68732493+anamaya-vyas-zlatan@users.noreply.github.com>
2022-02-01 21:44:29 +0530

    Create likePost.js

---
+++ server/controllers/post/likePost.js
@@ -0,0 +1,32 @@
+import Post from "../../models/Post.js";
+import User from "../../models/User.js";
+
+const likePost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id).select("likes liked_by");
+		if (!post) {
+			throw { status: 404, message: "Post not found" };
+		}
+		const user = await User.findById(req.body.id).select("username");
+		if (!user) {
+			throw { status: 404, message: "Please signup or login to continue" };
+		}
+
+		if (post.liked_by.includes(user.username)) {
+			return res.status(200).json(post);
+		}
+
+		post.likes = post.likes + 1;
+		post.liked_by.push(user.username);
+
+		await post.save();
+
+		res.status(200).json(post);
+	} catch (err) {
+		res
+			.status(err.status || 200)
+			.json({ message: res.message || "Something went wrong" });
+	}
+};
+
+export default likePost;
