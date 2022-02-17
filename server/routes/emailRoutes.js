d4286cd4f930c052f3ba8fdd21246372888f5129
ANAMAYA VYAS <68732493+anamaya-vyas-zlatan@users.noreply.github.com>
2022-02-17 11:28:35 +0530

    Delete emailRoutes.js

--- server/routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import sendEmail from "../controllers/email/sendEmail.js";
-
-const router = express.Router();
-
-router.post("/sendEmail", sendEmail);
-
-export default router;
